1092fae99d278c73e7e2964fb1953fecf7e8bc46
yl <araevmaksim681@gmail.com>
2023-01-12 16:41:26 +0300

    start

---
+++ test_reverse.py
@@ -0,0 +1,28 @@
+import pytest
+from yandex_testing_lesson import reverse
+
+
+def test_empty():
+    assert reverse('') == ''
+
+
+def test_one_symbol():
+    assert reverse('a') == 'a'
+
+
+def test_polindrome_word():
+    assert reverse('asa') == 'asa'
+
+
+def test_polindrome_stroke():
+    assert reverse('asa asa asa') == 'asa asa asa'
+
+
+def test_wrong_type():
+    with pytest.raises(TypeError):
+        reverse(42)
+
+
+def test_list():
+    with pytest.raises(TypeError):
+        reverse(reverse(['1', '2', '1']))
